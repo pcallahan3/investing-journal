b9742fe045a2399303343b4c12b4d58b7c269d50
Patrick <pcallahan3@mail.greenriver.edu>
2018-06-30 19:38:02 -0700

    Setup nav-bar with links to navigate through website

--- application/views/Templates/admin.php
+++ application/views/Templates/admin.php
@@ -7,10 +7,11 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Investing Journal | Admin Area</title>
+    <title>Starter Template for Bootstrap</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <!-- Custom styles for this template -->
     <link href="<?php echo base_url(); ?> assets/css/style.css" rel="stylesheet">
@@ -18,45 +19,26 @@
 
 <body>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="#">Investing Journal</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#"><?php echo anchor('admin', 'Dashboard', 'title="Dashboard Home"'); ?></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Investing Journal</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><?php echo anchor('admin', 'Dashboard', 'title="Dashboard Home"'); ?></li>
+            <li><?php echo anchor('admin/pages', 'Pages', 'title="View Pages"'); ?></li>
+            <li><?php echo anchor('admin/subjects', 'Subjects', 'title="View Subjects"'); ?></li>
+            <li><?php echo anchor('admin/users', 'Users', 'title="View Users"'); ?></li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><?php echo anchor('/', 'View Website', 'target="_blank"'); ?></li>
+            <li><?php echo anchor('users/logout', 'Logout', 'title="Logout"'); ?></li>
+        </ul>
     </div>
 </nav>
-
 <main role="main" class="container">
 
-    <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-    </div>
 
 </main><!-- /.container -->
 
