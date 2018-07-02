ac99a41c2bf2ed799d96a5195313d7eaf398ea8c
Patrick <pcallahan3@mail.greenriver.edu>
2018-07-02 16:24:20 -0700

    Autoloaded models in config/autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('Subject_model', 'Page_model', 'User_model', 'Activity_model');
